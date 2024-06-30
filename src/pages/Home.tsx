065cda13778120d8c5aa820d6e46da240c955999
Ghislain Pango <gpango@bmlcenter.com>
2024-06-30 15:59:11 +0100

    fix: remove unused import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,3 @@
-import { FaLinkedinIn } from "react-icons/fa6";
-import { IoMailOpenOutline } from "react-icons/io5";
-import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import AboutSection from "../components/AboutSection";
