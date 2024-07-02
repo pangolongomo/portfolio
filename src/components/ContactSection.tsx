3b17fd842519fae403f4d2ad4ccb49c06660e802
Ghislain Pango <gpango@bmlcenter.com>
2024-07-02 22:57:19 +0100

    feat:contact

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,24 +1,34 @@
 import { IoMailOpenOutline } from "react-icons/io5";
 import SectionContainer from "./ui/SectionContainer";
 import { FaLinkedinIn } from "react-icons/fa6";
+import ContentGroup from "./ui/ContentGroup";
 
 function ContactSection() {
   return (
     <SectionContainer subtitle="Get in Touch" title="Contact Me">
-      <div>
-        <ul>
-          <li>
-            <IoMailOpenOutline />
-            <span>pangolongomo@gmail.com</span>
-          </li>
-          <li>
-            <FaLinkedinIn />
-            <span>LinkedIn</span>
-          </li>
+      <ContentGroup>
+        <ul className="flex gap-3 md:gap-8 flex-col md:flex-row">
+          {contactList.map((item) => (
+            <li key={item.link} className="flex items-center gap-2">
+              <item.icon size={20} />
+              <span>{item.link}</span>
+            </li>
+          ))}
         </ul>
-      </div>
+      </ContentGroup>
     </SectionContainer>
   );
 }
 
 export default ContactSection;
+
+const contactList = [
+  {
+    link: "pangolongomo@gmail.com",
+    icon: IoMailOpenOutline,
+  },
+  {
+    link: "LinkedIn",
+    icon: FaLinkedinIn,
+  },
+];
